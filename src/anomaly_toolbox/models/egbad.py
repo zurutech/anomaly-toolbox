ec2a24806ee51f8234ed3097d41cc8816caea4b4
dimatfed <federico.d@zuru.tech>
2021-06-28 17:39:11 +0200

    :recycle: models/egbad.py, changed the model structure
    Now the structure is the same as for the original paper

--- src/anomaly_toolbox/models/egbad.py
+++ src/anomaly_toolbox/models/egbad.py
@@ -1,6 +1,7 @@
 """BiGAN Architecture Implementation as used in EGBAD."""
 
 import tensorflow.keras as k
+import tensorflow as tf
 
 KERNEL_INITIALIZER = k.initializers.RandomNormal(mean=0.0, stddev=0.02)
 ALMOST_ONE = k.initializers.RandomNormal(mean=1.0, stddev=0.02)
@@ -21,7 +22,7 @@ class Encoder(k.Model):
         Args:
             n_channels: depth of the input image
             latent_space_dimension: Dimension (along the depth) of the resulting
-                                    encoded represention.
+                                    encoded representation.
         Return:
             The assembled model.
         """
@@ -30,7 +31,7 @@ class Encoder(k.Model):
         l2_penalty = 0.0
         filters = 32
 
-        input_layer = k.layers.Input(shape=(32, 32, n_channels))
+        input_layer = k.layers.Input(shape=(28, 28, n_channels))
         x = k.layers.Conv2D(
             filters,
             kernel_size=4,
@@ -93,60 +94,64 @@ class Decoder(k.Model):
         Args:
             n_channels: depth of the output image.
             latent_space_dimension: Dimension (along the depth) of the resulting
-                                    encoded represention.
+                                    encoded representation.
         Return:
             The assembled model.
         """
         super().__init__()
 
-        filters = 32
+        # output dimension is 28, 28
         l2_penalty = 0.0
-        output_dimension = (32, 32, n_channels)
 
         input_layer = k.layers.Input(shape=(1, 1, latent_space_dimension))
 
-        x = k.layers.Conv2DTranspose(
-            filters,
-            kernel_size=4,
-            strides=1,
-            padding="valid",
-            use_bias=False,
-            kernel_regularizer=k.regularizers.l2(l2_penalty),
-            kernel_initializer=KERNEL_INITIALIZER,
-        )(input_layer)
-        x = k.layers.ReLU()(x)
+        x = k.layers.Dense(1024, activation="relu")(input_layer)
+        x = k.layers.BatchNormalization(
+            beta_initializer=ALMOST_ONE,
+            gamma_initializer=ALMOST_ONE,
+            momentum=0.1,
+            epsilon=1e-5,
+        )(x)
+        x = k.layers.Dense(7 * 7 * 128, activation="relu")(x)
+        x = k.layers.BatchNormalization(
+            beta_initializer=ALMOST_ONE,
+            gamma_initializer=ALMOST_ONE,
+            momentum=0.1,
+            epsilon=1e-5,
+        )(x)
 
-        vector_size = 4
-        while vector_size < output_dimension[0] // 2:
-            vector_size = vector_size * 2
-            filters = filters * 2
-            x = k.layers.Conv2DTranspose(
-                filters,
-                kernel_size=4,
-                strides=2,
-                padding="same",
-                use_bias=False,
-                kernel_regularizer=k.regularizers.l2(l2_penalty),
-                kernel_initializer=KERNEL_INITIALIZER,
-            )(x)
-            x = k.layers.BatchNormalization(
-                beta_initializer=ALMOST_ONE,
-                gamma_initializer=ALMOST_ONE,
-                momentum=0.1,
-                epsilon=1e-5,
-            )(x)
-            x = k.layers.ReLU()(x)
+        x = tf.reshape(x, [-1, 7, 7, 128])
 
-        # Construct the final layer
         x = k.layers.Conv2DTranspose(
-            output_dimension[-1],
+            64,
             kernel_size=4,
             strides=2,
             padding="same",
             use_bias=False,
-            activation="tanh",
+            kernel_regularizer=k.regularizers.l2(l2_penalty),
             kernel_initializer=KERNEL_INITIALIZER,
+            activation="relu",
         )(x)
+        x = k.layers.BatchNormalization(
+            beta_initializer=ALMOST_ONE,
+            gamma_initializer=ALMOST_ONE,
+            momentum=0.1,
+            epsilon=1e-5,
+        )(x)
+        x = k.layers.Conv2DTranspose(
+            1,
+            kernel_size=4,
+            strides=2,
+            padding="same",
+            use_bias=False,
+            kernel_regularizer=k.regularizers.l2(l2_penalty),
+            kernel_initializer=KERNEL_INITIALIZER,
+            activation="tanh",
+        )(x)
+
+        print("ciao")
+
+
 
         self._decoder = k.Model(input_layer, x, name="bigan_decoder")
 
@@ -169,12 +174,13 @@ class Discriminator(k.Model):
         Args:
             n_channels: depth of the output image.
             latent_space_dimension: Dimension (along the depth) of the resulting
-                                    encoded represention.
+                                    encoded representation.
         Return:
             The assembled model.
         """
         super().__init__()
         input_dimension = (28, 28, n_channels)
+        # input_dimension = (32, 32, 1)
 
         encoder = Encoder(n_channels, latent_space_dimension)
         input_layer = k.layers.Input(shape=input_dimension)
