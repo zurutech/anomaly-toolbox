4233b9d973580fdeb6b8accf37fd9568cacfc0d0
dimatfed <federico.d@zuru.tech>
2021-07-16 13:05:21 +0200

    :construction: models/ganomaly_2.py, first version of new model
    To susbstitute the old one

---
+++ src/anomaly_toolbox/models/ganomaly_2.py
@@ -0,0 +1,140 @@
+import tensorflow.keras as k
+
+KERNEL_INITIALIZER = k.initializers.RandomNormal(mean=0.0, stddev=0.02)
+ALMOST_ONE = k.initializers.RandomNormal(mean=1.0, stddev=0.02)
+
+class Generator(k.Sequential):
+    def __init__(
+        self,
+        n_channels: int = 3,
+        latent_space_dimension: int = 100,
+        l2_penalty: float = 0.0,
+    ):
+        super().__init__(
+            [
+                k.layers.Input(shape=latent_space_dimension),
+                k.layers.Dense(512, kernel_initializer=KERNEL_INITIALIZER),
+                k.layers.Dense(7 * 7 * 128, kernel_initializer=KERNEL_INITIALIZER),
+                k.layers.BatchNormalization(
+                    beta_initializer=ALMOST_ONE,
+                    gamma_initializer=ALMOST_ONE,
+                    momentum=0.1,
+                    epsilon=1e-5,
+                ),
+                k.layers.LeakyReLU(0.2),
+                k.layers.Reshape(target_shape=(7, 7, 128)),
+                k.layers.Conv2DTranspose(
+                    filters=256,
+                    kernel_size=(4, 4),
+                    strides=(1, 1),
+                    padding="valid",
+                    kernel_initializer=KERNEL_INITIALIZER,
+                    use_bias=False,
+                    kernel_regularizer=k.regularizers.l2(l2_penalty),
+                ),
+                k.layers.BatchNormalization(),
+                k.layers.LeakyReLU(0.2),
+                k.layers.Conv2DTranspose(
+                    filters=128,
+                    kernel_size=(4, 4),
+                    strides=(2, 2),
+                    padding="same",
+                    kernel_initializer=KERNEL_INITIALIZER,
+                    use_bias=False,
+                    kernel_regularizer=k.regularizers.l2(l2_penalty),
+                ),
+                k.layers.BatchNormalization(),
+                k.layers.LeakyReLU(0.2),
+                k.layers.Conv2DTranspose(
+                    filters=64,
+                    kernel_size=(4, 4),
+                    strides=(2, 2),
+                    padding="same",
+                    kernel_initializer=KERNEL_INITIALIZER,
+                    use_bias=False,
+                    kernel_regularizer=k.regularizers.l2(l2_penalty),
+                ),
+                k.layers.BatchNormalization(),
+                k.layers.LeakyReLU(0.2),
+                k.layers.Conv2DTranspose(
+                    filters=3,
+                    kernel_size=(4, 4),
+                    strides=(2, 2),
+                    padding="same",
+                    kernel_initializer=KERNEL_INITIALIZER,
+                    use_bias=False,
+                    kernel_regularizer=k.regularizers.l2(l2_penalty),
+                ),
+                k.layers.Activation(k.activations.tanh),
+            ]
+        )
+
+
+class Discriminator(k.Model):
+    def __init__(
+        self,
+        n_channels: int = 3,
+        l2_penalty: float = 0.0,
+    ):
+        super().__init__()
+
+        # Input
+        input_dimension = (28, 28, n_channels)
+        self._input_layer = k.layers.InputLayer(input_shape=input_dimension)
+
+        self._backbone = k.Sequential(
+            [
+                self._input_layer,
+                k.layers.Conv2D(
+                    filters=64,
+                    kernel_size=(4, 4),
+                    strides=(2, 2),
+                    padding="same",
+                    kernel_initializer=KERNEL_INITIALIZER,
+                    use_bias=False,
+                    kernel_regularizer=k.regularizers.l2(l2_penalty),
+                ),
+                k.layers.LeakyReLU(0.2),
+                k.layers.Conv2D(
+                    filters=128,
+                    kernel_size=(4, 4),
+                    strides=(2, 2),
+                    padding="same",
+                    kernel_initializer=KERNEL_INITIALIZER,
+                    use_bias=False,
+                    kernel_regularizer=k.regularizers.l2(l2_penalty),
+                ),
+                k.layers.LeakyReLU(0.2),
+                k.layers.Conv2D(
+                    filters=256,
+                    kernel_size=(4, 4),
+                    strides=(2, 2),
+                    padding="same",
+                    kernel_initializer=KERNEL_INITIALIZER,
+                    use_bias=False,
+                    kernel_regularizer=k.regularizers.l2(l2_penalty),
+                ),
+                k.layers.LeakyReLU(0.2),
+            ]
+        )
+
+        self._output = k.Sequential(
+            [
+                k.layers.Conv2D(
+                    filters=1,
+                    kernel_size=(4, 4),
+                    strides=(2, 2),
+                    padding="same",
+                    kernel_initializer=KERNEL_INITIALIZER,
+                    use_bias=False,
+                    kernel_regularizer=k.regularizers.l2(l2_penalty),
+                ),
+            ]
+        )
+
+    def call(self, inputs, training=True, mask=None):
+
+        features = self._backbone(inputs)
+        output = self._output(features)
+
+        return output, features
